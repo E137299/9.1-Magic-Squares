a0d0e0a1ef0c3760c1eab23b6e8dbb979a4da206
David Yin <29613693+E137299@users.noreply.github.com>
2025-11-07 10:12:00 -0600

    Implement reading and printing square data
    Added functionality to read and process square data from a file.

--- Main.java
+++ Main.java
@@ -4,8 +4,23 @@ import java.io.IOException;
 
 public class Main{
   public static void main(String[] args){
-      Scanner scan = new Scanner(new File("magicData"));
+      Scanner scan = new Scanner(new File("magicData.txt"));
+      int count = 1; // Counter for square number
+      int size = scan.nextInt(); // Read size of the first square
 
+      while (size != -1) {
+          Square square = new Square(size); // Create new Square
+          square.readSquare(scan); // Read values into the square
+
+          System.out.println("\n******** Square " + count + " ********");
+          square.printSquare();
+
+          // Print sums of rows, columns, and diagonals
+
+
+          // Check and print if the square is magic
+      }
   }
 }
+
 
